6c3a557db9a610a2218679783bc97121704df905
zuly9 <olartezuly9@gmail.com>
2025-07-29 16:18:57 -0500

    Cambios
    Se agregan proyectos y nuevas clases

---
+++ js/header.js
@@ -0,0 +1,9 @@
+document.querySelector('.encabezado-principal').innerHTML = `
+    <h1>IED NOBEL JUAN MANUEL SANTOS</h1>
+    <nav>
+        <a href="../index.html">Inicio</a>
+        <a href="../vistas/registrar_estudiante.html">Registro</a>
+        <a href="../vistas/contacto.html">Contacto</a>
+        <a href="../vistas/login.html">Login</a>
+    </nav>
+`;
