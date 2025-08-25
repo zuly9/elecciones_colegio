78db7a8aca54f8657eae6314fc45a1b91ed9e8f7
zuly9 <olartezuly9@gmail.com>
2025-08-25 15:27:59 -0500

    Registro login

---
+++ js/login.js
@@ -0,0 +1,21 @@
+       document.addEventListener("DOMContentLoaded", () => {
+    const loginForm = document.getElementById("loginForm");
+    const mensajeError = document.getElementById("mensajeError");
+
+    loginForm.addEventListener("submit", function (e) {
+         e.preventDefault();
+
+         const usuario = document.getElementById("usuario").value.trim();
+         const contraseña = document.getElementById("contraseña").value.trim();
+
+        if (usuario === "admin" && contraseña === "123") {
+            window.location.href = "../vistas/menu_principal.html";
+        } else {
+            mensajeError.textContent = "Usuario o contraseña incorrectos";
+            mensajeError.style.display ="block";
+        }
+    });
+});
+
+
+       
